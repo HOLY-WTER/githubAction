da000ba50ed69063f3b6a519770b848bfd55d62d
HOLY-WTER <monmonten@gmail.com>
2023-04-18 00:08:06 +0700

    second test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+def test(x):
+    if x==1:
+        print("adasd")
+    if x==2:
+        print("adasd")
+    if x==3:
+        print("adasd")
+
+def main():
+    test(2)
+if __name__ == "__main__":
+    main()
