34857e2520a13019fc154d6d2e2a7a92621fc52c
HOLY-WTER <monmonten@gmail.com>
2023-04-18 10:44:23 +0700

    test.py fourth commit

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 def check_num(num):
-    if num == 1:
-       return "One"
-    if num == 2:
-       return "Two"
-    if num == 3:
-       return 'Three'
+    if num==1:
+        return "One"
+    if num==2:
+        return "Two"
+    if num==3:
+        return "Three"
 
 def main():
     check_num(2)
+
+
 if __name__ == "__main__":
+    
     main()
